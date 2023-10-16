2d97fb4a5fa862048ef5937f713f1e3e42ae8abf
qawatake <38106890+qawatake@users.noreply.github.com>
2023-10-16 19:56:37 +0900

    breaking

--- bench_test.go
+++ bench_test.go
@@ -12,24 +12,24 @@ type key struct{}
 func BenchmarkWithKeyValue(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		ctx := context.Background()
-		ctx = ctxvls.WithKeyValues(ctx, key{}, 10)
+		ctx = ctxvls.WithValues(ctx, key{}, 10)
 	}
 }
 
 func BenchmarkWithKeyValues(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		ctx := context.Background()
-		ctx = ctxvls.WithKeyValues(ctx, key{}, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+		ctx = ctxvls.WithValues(ctx, key{}, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 	}
 }
 
 func BenchmarkValuesFromByKey(b *testing.B) {
 	ctx := context.Background()
 	for j := 0; j < 10; j++ {
-		ctx = ctxvls.WithKeyValues(ctx, key{}, j)
+		ctx = ctxvls.WithValues(ctx, key{}, j)
 	}
 	for i := 0; i < b.N; i++ {
-		ctxvls.ValuesFromByKey(ctx, key{})
+		ctxvls.ValuesFrom[key, int](ctx, key{})
 	}
 }
 
@@ -37,7 +37,7 @@ func BenchmarkWithKeyValue_multiple(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		ctx := context.Background()
 		for j := 0; j < 1000; j++ {
-			ctx = ctxvls.WithKeyValues(ctx, key{}, j)
+			ctx = ctxvls.WithValues(ctx, key{}, j)
 		}
 	}
 }
@@ -46,7 +46,7 @@ func BenchmarkWithKeyValues_multiple(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		ctx := context.Background()
 		for j := 0; j < 1000; j++ {
-			ctx = ctxvls.WithKeyValues(ctx, key{}, j+1, j+2, j+3, j+4, j+5, j+6, j+7, j+8, j+9, j+10)
+			ctx = ctxvls.WithValues(ctx, key{}, j+1, j+2, j+3, j+4, j+5, j+6, j+7, j+8, j+9, j+10)
 		}
 	}
 }
--- ctxvls.go
+++ ctxvls.go
@@ -7,27 +7,12 @@ import (
 	ctxvls "github.com/qawatake/ctxvls/internal/ctxvls3"
 )
 
-// WithKeyValues returns a copy of parent in which values are associated with key.
-// WithKeyValues is used together with ValuesFromByKey.
-func WithKeyValues[T comparable](ctx context.Context, key T, values ...any) context.Context {
+// WithValues returns a copy of parent in which a collection of values are associated with a key.
+func WithValues[T comparable, V any](ctx context.Context, key T, values ...V) context.Context {
 	return ctxvls.WithKeyValues(ctx, key, values...)
 }
 
-// ValuesFromByKey returns all the values associated with key and stored by WithKeyValues.
-func ValuesFromByKey[T comparable](ctx context.Context, key T) []any {
-	return ctxvls.ValuesFrom[T, any](ctx, key)
+// ValuesFrom returns all the values associated with key.
+func ValuesFrom[T comparable, V any](ctx context.Context, key T) []V {
+	return ctxvls.ValuesFrom[T, V](ctx, key)
 }
-
-// WithValues returns a copy of parent that stores values.
-// WithValues[T] is used together with ValuesFrom[T].
-func WithValues[T any](ctx context.Context, values ...T) context.Context {
-	return ctxvls.WithKeyValues(ctx, partitionKey[T]{}, values...)
-}
-
-// ValuesFrom returns all the values of type T stored by WithValues.
-// Note that ValuesFrom does not return values stored by WithKeyValues even if value types are the same.
-func ValuesFrom[T any](ctx context.Context) []T {
-	return ctxvls.ValuesFrom[partitionKey[T], T](ctx, partitionKey[T]{})
-}
-
-type partitionKey[T any] struct{}
