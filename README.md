19b34c1dcc03f7705ba2f4b7be785f7499187cfd
qawatake <38106890+qawatake@users.noreply.github.com>
2023-10-16 09:32:17 +0900

    update doc

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 [![Go Reference](https://pkg.go.dev/badge/github.com/qawatake/ctxvls.svg)](https://pkg.go.dev/github.com/qawatake/ctxvls)
 [![test](https://github.com/qawatake/ctxvls/actions/workflows/test.yaml/badge.svg)](https://github.com/qawatake/ctxvls/actions/workflows/test.yaml)
 
-Package `ctxvls` associate multiple values with a key and store them in context, and it is designed to be safe for concurrent use.
+Package `ctxvls` provide APIs for storing and retrieving collections of values in context, and it is designed to be safe for concurrent use.
 
 ```go
 package main
--- ctxvls.go
+++ ctxvls.go
@@ -1,4 +1,4 @@
-// Package ctxvls associate multiple values with a key and store them in context, and it is designed to be safe for concurrent use.
+// Package ctxvls provide APIs for storing and retrieving collections of values in context, and it is designed to be safe for concurrent use.
 package ctxvls
 
 import (
@@ -7,22 +7,24 @@ import (
 	ctxvls "github.com/qawatake/ctxvls/internal/ctxvls3"
 )
 
-// WithKeyValues returns a copy of parent that stores values for key.
+// WithKeyValues returns a copy of parent in which values are associated with key.
+// WithKeyValues is used together with ValuesFromByKey.
 func WithKeyValues[T comparable](ctx context.Context, key T, values ...any) context.Context {
 	return ctxvls.WithKeyValues(ctx, key, values...)
 }
 
-// ValuesFromByKey returns all the values stored for key by WithKeyValues.
+// ValuesFromByKey returns all the values associated with key and stored by WithKeyValues.
 func ValuesFromByKey[T comparable](ctx context.Context, key T) []any {
 	return ctxvls.ValuesFrom[T, any](ctx, key)
 }
 
 // WithValues returns a copy of parent that stores values.
+// WithValues[T] is used together with ValuesFrom[T].
 func WithValues[T any](ctx context.Context, values ...T) context.Context {
 	return ctxvls.WithKeyValues(ctx, partitionKey[T]{}, values...)
 }
 
-// ValuesFrom returns all the values stored for type T by WithValues.
+// ValuesFrom returns all the values of type T stored by WithValues.
 // Note that ValuesFrom does not return values stored by WithKeyValues even if value types are the same.
 func ValuesFrom[T any](ctx context.Context) []T {
 	return ctxvls.ValuesFrom[partitionKey[T], T](ctx, partitionKey[T]{})
